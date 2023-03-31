9adb2ac20ec2105384329074fc45ba08c7c6f014
MrBattka <110553715+MrBattka@users.noreply.github.com>
2023-03-31 18:42:13 +0300

    styled mob profile all 1.0

---
+++ src/components/Mobile/Profile/MyPost/MyPost.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Preloader from '../../../Common/Preloader/Preloader';
+import Posts from '../Posts/Posts';
+import classes from './MyPost.module.css';
+import { MyPostReduxForm } from './MyPostForm/MyPostForm';
+
+const MyPosts = (props) => {
+
+    const onPostMessage = (values) => {
+        if (values.newPostText) {
+            props.addPost(values.newPostText, props.id)
+            values.newPostText = ''
+        }
+    }
+
+    return (
+        <div className={classes.wrapper}>
+            <MyPostReduxForm onSubmit={onPostMessage} addPost={props.addPost} newPostText={props.newPostText} />
+            <h2>Posts:</h2>
+            <div className={classes.posts}>
+                {props.isFetching ?
+                    <Posts posts={props.posts} id={props.id} deletePost={props.deletePost} profile={props.profile} idPost={props.idPost} />
+                    : <Preloader />}
+            </div>
+        </div>
+    )
+}
+
+export default MyPosts;
--- src/components/Mobile/Profile/Profile.jsx
+++ src/components/Mobile/Profile/Profile.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import Preloader from '../../Common/Preloader/Preloader'
+import AdditionalInformation from './AdditionalInformation/AdditionalInformation'
+import MyPostsContainer from './MyPost/MyPostContainer'
 import classes from './Profile.module.css'
 import ProfileInfo from './ProfileInfo/ProfileInfo'
 
 const Profile = ({ profile, status, authUserId, isAuth, urlProfilePhoto, getProfile,
-                updateProfileStatus, getProfileStatus, updateProfilePhoto }) => {
+                updateProfileStatus, getProfileStatus, updateProfilePhoto, lookingForAJob,
+                lookingForAJobDescription, fullName, contacts }) => {
   if (!profile) {
     return <Preloader />
   }
@@ -13,10 +16,10 @@ const Profile = ({ profile, status, authUserId, isAuth, urlProfilePhoto, getProf
     <div className={classes.profile__wrapper}>
       <ProfileInfo profile={profile} status={status}
         updateProfileStatus={updateProfileStatus} urlProfilePhoto={urlProfilePhoto} updateProfilePhoto={updateProfilePhoto} />
-      {/* <AdditionalInformation profile={profile} lookingForAJob={lookingForAJob}
+      <AdditionalInformation profile={profile} lookingForAJob={lookingForAJob}
         lookingForAJobDescription={lookingForAJobDescription}
-        fullName={fullName} contacts={contacts} /> */}
-      {/* <MyPostsContainer profile={profile} /> */}
+        fullName={fullName} contacts={contacts} />
+      <MyPostsContainer profile={profile} />
     </div>
   )
 }
